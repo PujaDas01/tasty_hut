edb1a05652d801c692edd8d9681dea013423fb25
Puja Das <102240706+PujaDas@users.noreply.github.com>
2023-06-16 15:28:11 +0530

    Changed code of regex

--- src/pages/authentication/SignUp.js
+++ src/pages/authentication/SignUp.js
@@ -39,7 +39,7 @@ const onSignUpHandler = () => {
 }
 
 const emailValidator = (value) => {
-  const regexEmail = /^\w+([\.-]?\w+)@\w+([\.-]?\w+)(\.\w{2,3})+$/;
+  const regexEmail = /^\w+([.-]?\w+)@\w+([.-]?\w+)(\.\w{2,3})+$/;
   if(regexEmail.test(value)) {
     return true;
   } else {
