ab08fcc220cccce00dab81a59f828af1d5a52767
Puja Das <102240706+PujaDas@users.noreply.github.com>
2024-01-29 18:34:17 +0530

    Added content for About Page

--- src/pages/primary_screens/About.js
+++ src/pages/primary_screens/About.js
@@ -1,8 +1,53 @@
 import React from 'react';
+import Footer from '../../components/Footer';
+import BgImage from '../../components/BgImage';
 
 const About = () => {
   return (
-    <div className='commonWrapper'>About</div>
+    <>
+      <div className='commonWrapper'>
+        <section className='aboutContainer'>
+          <div className='aboutContent'>
+            <h1>About Us</h1>
+            <b>Welcome to TastyHut – Your Culinary Haven!</b>
+            <p>At TastyHut, we believe that cooking is an art, a delightful journey that transforms fresh ingredients into scrumptious masterpieces. Our mission is to inspire and guide you through this gastronomic adventure, offering a treasure trove of delectable recipes, cooking tips, and culinary inspiration.<br/><br/>Step into the enchanting world of TastyHut, where the kitchen is a canvas and every recipe is a stroke of culinary brilliance. We believe in the magic that happens when simple ingredients are transformed into extraordinary dishes, and we're here to be your compass on this flavorful journey. Our commitment goes beyond mere recipes; we're dedicated to cultivating a community of passionate food enthusiasts. At TastyHut, you're not just a visitor; you're part of a family that shares the joy of creating and savoring incredible meals together.<br/><br/>Discover the artistry in every dish as we unravel the secrets of seasoned chefs and home cooks alike. From kitchen hacks that simplify your cooking process to innovative twists that redefine traditional favorites, TastyHut is your hub for continuous culinary inspiration.<br/><br/>Dive into our culinary haven, where we believe that a well-prepared meal is a symphony for the senses. Feel the satisfaction of chopping, the melody of simmering, and the crescendo of flavors as you create memorable experiences one bite at a time.Our recipes are more than just instructions; they are invitations to explore, experiment, and make each dish uniquely yours. Let the pages of TastyHut be your companion in the kitchen, guiding you through the delightful maze of tastes and textures.<br/><br/>At TastyHut, we celebrate the connection between food and emotions. From the warmth of comfort food to the excitement of trying something new, we invite you to forge your own culinary path with us. Join our community, and let's embark on a culinary adventure where every recipe is a story waiting to be told, tasted, and shared.
+            </p>
+          </div>
+          <div className='aboutImageContent'>
+            <BgImage
+              imageSource='pic3.jpg'
+            />
+          </div>
+        </section>
+        <section className='aboutContainer'>
+        <div className='aboutImageContent'>
+            <BgImage
+              imageSource='chef.png'
+            />
+          </div>
+          <div className='aboutContent'>
+            <b>🌟 Explore Endless Culinary Possibilities:</b>
+            <p className='paraSpace'>
+            Dive into our vast collection of mouthwatering recipes that cater to every taste and occasion. From quick and easy weeknight dinners to show-stopping desserts, we've got your cravings covered. Our diverse range of recipes spans global cuisines, ensuring there's always something new to discover and savor.
+            </p>
+            <b>👩‍🍳 Expert Tips and Tricks:</b>
+            <p className='paraSpace'>Whether you're a seasoned chef or a kitchen novice, TastyHut is your go-to source for culinary wisdom. Our expert tips and tricks will elevate your cooking skills, turning your kitchen into a culinary playground where innovation and flavor collide.</p>
+            <b>🌐 Connect with a Vibrant Community:</b>
+            <p className='paraSpace'>Join our passionate community of food enthusiasts, where you can share your culinary triumphs, seek advice, and connect with like-minded individuals who share your love for great food. Exchange ideas, discuss techniques, and be a part of a global network that celebrates the joy of cooking.</p>
+            <b>🍲 Personalized Meal Planning:</b>
+            <p className='paraSpace'>Struggling with meal planning? TastyHut has you covered! Browse through our curated meal plans tailored to various dietary preferences, making it easier than ever to prepare delicious and nutritious meals for yourself and your loved ones.</p>
+            <b>📸 Visual Feast of Inspiration:</b>
+            <p className='paraSpace'>Immerse yourself in a visual feast of stunning food photography and step-by-step cooking guides. Each recipe comes to life through vibrant images that inspire you to recreate the dish with confidence and creativity.</p>
+            <b>🧡 Passion for Quality Ingredients:</b>
+            <p className='paraSpace'>At TastyHut, we believe in the power of quality ingredients. Discover the importance of using fresh, seasonal produce, and learn how to make the most of pantry staples to create memorable meals that leave a lasting impression.</p>
+            <b>🌿 Cooking for a Better Tomorrow:</b>
+            <p className='paraSpace'>We're committed to promoting sustainable and mindful cooking practices. Find eco-friendly tips, explore plant-based recipes, and join us on a journey towards a healthier, more sustainable future.</p>
+            <p>Embark on your culinary adventure with TastyHut – where every recipe is a story waiting to be shared, and every meal is an expression of love. Happy cooking!</p>
+          </div>
+        </section>
+      </div>
+      <Footer/>
+    </>
   )
 }
 
